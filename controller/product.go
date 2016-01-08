e91b4561e60b0489af303be9af6dda3c3b94d18c
Shunchao Wang <wang.2799@osu.edu>
2016-01-08 08:41:37 -0500

    Finished zencart query.

--- controller/product.go
+++ controller/product.go
@@ -33,7 +33,29 @@ func NewProductController(db *sql.DB) *ProductController {
 
 func (pc ProductController) Get(id int) model.Product {
 
-	return model.Product{}
+	query := fmt.Sprintf("%s WHERE p.products_id = %d", productQuery, id)
+	row := pc.db.QueryRow(query)
+	defer pc.db.Close()
+	var product_id int
+	var product_model string
+	var quantity int
+	var image string
+	var price float32
+	var weight float32
+	var name string
+	var description string
+
+	var category_id int
+	var category_image string
+	var category_name string
+	var category_description string
+
+	err := row.Scan(&product_id, &product_model, &quantity, &image, &price, &weight, &name, &description, &category_id, &category_image, &category_name, &category_description)
+	if err != nil {
+		panic(err)
+	}
+	product := model.Product{product_id, product_model, quantity, image, price, weight, name, description, model.Category{category_id, category_image, category_name, category_description}}
+	return product
 }
 
 func (pc ProductController) Create(p model.Product) model.Product {
@@ -186,16 +208,76 @@ func (pc ProductController) Query(args ...interface{}) []model.Product {
 	return products
 }
 
+/*
+ * Get products by category.
+ * Support 1 argument or 3 argument, 1st is category id, 2nd limit and 3rd offset are for pagination.
+ */
+func (pc ProductController) GetProductsByCategory(category_id int, args ...interface{}) []model.Product {
+	var query string
+	if len(args) == 2 {
+		limit, ok := args[0].(int)
+		if !ok {
+			panic("1st parameter not type int.")
+		}
+		offset, ok := args[1].(int)
+		if !ok {
+			panic("2nd parameter not type int.")
+		}
+		query = fmt.Sprintf("%s WHERE c.categories_id = %d LIMIT %d OFFSET %d", productQuery, category_id, limit, offset)
+	} else {
+		query = fmt.Sprintf("%s WHERE c.categories_id = %d", productQuery, category_id)
+	}
+	rows, err := pc.db.Query(query)
+	if err != nil && rows == nil {
+		return nil
+	}
+	defer pc.db.Close()
+	products := make([]model.Product, 0)
+	for rows.Next() {
+		var id int
+		var product_model string
+		var quantity int
+		var image string
+		var price float32
+		var weight float32
+		var name string
+		var description string
+
+		var category_id int
+		var category_image string
+		var category_name string
+		var category_description string
+
+		err = rows.Scan(&id, &product_model, &quantity, &image, &price, &weight, &name, &description, &category_id, &category_image, &category_name, &category_description)
+		if err != nil {
+			panic(err)
+		}
+		products = append(products, model.Product{id, product_model, quantity, image, price, weight, name, description, model.Category{category_id, category_image, category_name, category_description}})
+	}
+	return products
+}
+
 // Category Controller apis
 func NewCategoryController(db *sql.DB) *CategoryController {
 	return &CategoryController{db}
 }
-func (cc CategoryController) Get(id int) model.Category {
-	return model.Category{}
-}
 
-func (cc CategoryController) Query(s string) []model.Category {
-	return []model.Category{}
+func (cc CategoryController) Get(id int) model.Category {
+	query := fmt.Sprintf("%s WHERE c.categories_id = %d", categoryQuery, id)
+	row := cc.db.QueryRow(query)
+	defer cc.db.Close()
+	var category_id int
+	var image string
+	var name string
+	var description string
+
+	err := row.Scan(&category_id, &image, &name, &description)
+
+	if err != nil {
+		panic(err)
+	}
+	category := model.Category{id, image, name, description}
+	return category
 }
 
 func (cc CategoryController) GetAll() []model.Category {
@@ -213,6 +295,72 @@ func (cc CategoryController) GetAll() []model.Category {
 
 		err = rows.Scan(&id, &image, &name, &description)
 
+		if err != nil {
+			panic(err)
+		}
+		categories = append(categories, model.Category{id, image, name, description})
+	}
+	return categories
+}
+
+/*
+ * Query supports up to 3 args, query criteria, pagination.
+ * 1 arg: must be string, search product by name or description
+ * 2 args: must be 2 ints, the first one is page size, the second one is page number
+ * 3 args: must be string, int, int. Combine up 2.
+ */
+func (cc CategoryController) Query(args ...interface{}) []model.Category {
+	// Get any parameters passed out of the args variable
+	var query string
+	switch len(args) {
+	case 0: // query all
+		query = categoryQuery
+	case 1: // query criteria
+		criteria, ok := args[0].(string)
+		if !ok {
+			panic("1st parameter not type string.")
+		}
+		query = fmt.Sprintf("%s WHERE cd.categories_name LIKE '%%s%%'", categoryQuery, criteria, criteria)
+	case 2: // paged
+		limit, ok := args[0].(int)
+		if !ok {
+			panic("1st parameter not type int.")
+		}
+		offset, ok := args[1].(int)
+		if !ok {
+			panic("2nd parameter not type int.")
+		}
+		query = fmt.Sprintf("%s LIMIT %d OFFSET %d", categoryQuery, limit, offset)
+	case 3: // paged query criteria
+		criteria, ok := args[0].(string)
+		if !ok {
+			panic("1st parameter not type string.")
+		}
+		limit, ok := args[1].(int)
+		if !ok {
+			panic("2nd parameter not type int.")
+		}
+		offset, ok := args[2].(int)
+		if !ok {
+			panic("3rd parameter not type int.")
+		}
+		query = fmt.Sprintf("%s WHERE cd.categories_name LIKE '%%%s%%' LIMIT %d OFFSET %d", categoryQuery, criteria, criteria, limit, offset)
+	default:
+		query = categoryQuery
+	}
+	rows, err := cc.db.Query(query)
+	if err != nil && rows == nil {
+		return nil
+	}
+	defer cc.db.Close()
+	categories := make([]model.Category, 0)
+	for rows.Next() {
+		var id int
+		var image string
+		var name string
+		var description string
+
+		err = rows.Scan(&id, &image, &name, &description)
 		if err != nil {
 			panic(err)
 		}
--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
 
 	// third party packages
 	_ "github.com/go-sql-driver/mysql"
@@ -19,20 +20,72 @@ import (
 func main() {
 	router := httprouter.New()
 
-	router.GET("/queryProduct", func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	router.GET("/product/query", func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
 		pc := controller.NewProductController(getMysqlDB())
-		//products := pc.Query("SELECT products_id, products_model, type_id, type_name FROM products, product_types WHERE products.products_type = product_types.type_id LIMIT 10 OFFSET 5")
 		products := pc.Query()
 		psj, _ := json.Marshal(products)
 
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(200)
 		fmt.Fprintf(w, "%s", psj)
-		/*for _, product := range products {
-
-		  }*/
 	})
 
+	router.GET("/product/get", func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+		pc := controller.NewProductController(getMysqlDB())
+		idParam := r.FormValue("id")
+		id, err := strconv.Atoi(idParam)
+		if err != nil {
+			panic("id not converted to type int.")
+		}
+		product := pc.Get(id)
+		pj, _ := json.Marshal(product)
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(200)
+		fmt.Fprintf(w, "%s", pj)
+	})
+
+	router.GET("/category/products", func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+		pc := controller.NewProductController(getMysqlDB())
+		idParam := r.FormValue("cid")
+		id, err := strconv.Atoi(idParam)
+		if err != nil {
+			panic("id not converted to type int.")
+		}
+		products := pc.GetProductsByCategory(id)
+		psj, _ := json.Marshal(products)
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(200)
+		fmt.Fprintf(w, "%s", psj)
+	})
+
+	router.GET("/category/get", func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+		cc := controller.NewCategoryController(getMysqlDB())
+		idParam := r.FormValue("id")
+		id, err := strconv.Atoi(idParam)
+		if err != nil {
+			panic("id not converted to type int.")
+		}
+		category := cc.Get(id)
+		cj, _ := json.Marshal(category)
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(200)
+		fmt.Fprintf(w, "%s", cj)
+	})
+
+	router.GET("/category/query", func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+		cc := controller.NewCategoryController(getMysqlDB())
+		categories := cc.Query()
+		csj, _ := json.Marshal(categories)
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(200)
+		fmt.Fprintf(w, "%s", csj)
+	})
+
+	//TODO: DELETE
 	router.GET("/pagedProducts", func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
 		pc := controller.NewProductController(getMysqlDB())
 		//products := pc.Query("SELECT products_id, products_model, type_id, type_name FROM products, product_types WHERE products.products_type = product_types.type_id LIMIT 10 OFFSET 5")
@@ -47,6 +100,7 @@ func main() {
 		  }*/
 	})
 
+	//TODO: DELETE
 	router.GET("/allCategories", func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
 		cc := controller.NewCategoryController(getMysqlDB())
 		//products := pc.Query("SELECT products_id, products_model, type_id, type_name FROM products, product_types WHERE products.products_type = product_types.type_id LIMIT 10 OFFSET 5")
